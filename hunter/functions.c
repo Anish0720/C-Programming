bb081222a9b5191cc3c43f589ac30b5c39e3a93b
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2017-12-19 23:17:13 +0530

    Create functions.c

---
+++ hunter/functions.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int max(int b[],int l)
+{
+int pp=0;
+int m;
+m=b[pp];
+int i;
+for(i=1;i<l;i++)
+{
+if(b[i]>m)
+{
+m=b[i];
+}
+}
+return m;
+}
+int main(void){
+int n,i,max;
+scanf("&d",&a[i]);
+}
+max=max(a,n);
+printf("%d",max)'
+}
+
