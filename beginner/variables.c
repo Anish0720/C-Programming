c1a1d8e3607df333ac36b63865ffefad7753cbcd
Anish0720 <34673693+Anish0720@users.noreply.github.com>
2017-12-19 23:10:28 +0530

    Create variables.c

---
+++ beginner/variables.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char a[25]=Sachin Ramesh Tendulkar;
+char b[20]= Little Master;
+int c=18426;
+float d= 44.83;
+int e=463;
+printf("\n %c",a[23]);
+printf("\n %c",b[20]);
+printf("\n %d",c);
+printf("\n %f",d);
+printf("\n %d",e);
+}
